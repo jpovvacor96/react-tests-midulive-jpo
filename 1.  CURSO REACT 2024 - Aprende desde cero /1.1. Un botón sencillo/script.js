3339032dc07e4655872ebde0c8aa65edce66f547
jpovvacor96 <jpovvacor96>
2024-06-18 18:24:16 +0200

    Añadido material y pruebas sobre el primer vídeo

---
+++ 1.  CURSO REACT 2024 - Aprende desde cero /1.1. Un botón sencillo/script.js
@@ -0,0 +1,15 @@
+const button=document.querySelector('button')
+
+button.addEventListener('click', function(){
+
+    // Obtenemos el id del objeto sobre el que pulsamos
+    const id=button.getAttribute('data-id')
+
+    if (button.classList.contains('liked')){
+        button.classList.remove('liked')
+        button.innerText='Me gusta'
+    } else{
+        button.classList.add('liked')
+        button.innerText='No me gusta'
+    }
+})
---
+++ 1.  CURSO REACT 2024 - Aprende desde cero /1.4. Primer proyecto React con Vite/vite-project/src/TwitterFollowCard.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+export function TwitterFollowCard ({userName, name}){
+    return(
+        <article className='tw-followCard'>
+            <header className='tw-followCard-header'>
+                <img className='tw-followCard-avatar' src={`https://unavatar.io/${userName}`} alt="Avatar de Pedro" />
+                <div className='tw-followCard-info'>
+                    <strong>{name}</strong>
+                    <span className='tw-followCard-infoUserName'>@{userName}</span>
+                </div>
+            </header>
+            <aside>
+                <button className='tw-followCard-button'>
+                    Seguir
+                </button>
+            </aside>
+        </article>
+    )
+}
